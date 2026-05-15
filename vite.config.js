1e84ef384ee0dcadb6eb2242f483159a99d4f0ab
Safronjos <guerralovatojosue@hotmail.com>
2026-05-15 00:57:06 -0500

    cambio produccion

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
-
-  // IMPORTANTE: asegura rutas correctas en producción
   base: './',
-
-  // IMPORTANTE: todo lo que esté en /public se copia a dist
   publicDir: 'public',
 
   build: {
@@ -17,9 +13,5 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-  },
-
-  preview: {
-    port: 4173,
   },
 })
